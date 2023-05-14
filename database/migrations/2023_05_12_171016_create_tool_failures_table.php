37bf3b90e0001bd8cfde776f1228bbe319a198be
Zacharias Hendrik <zacha.jubilee@gmail.com>
2023-05-14 21:49:48 +0700

    some models

---
+++ database/migrations/2023_05_12_171016_create_tool_failures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tool_failures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tool_id');
+            $table->string('type');
+            $table->date('date');
+            $table->string('action_taken');
+            $table->foreign('tool_id')->references('id')->on('tools')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tool_failures');
+    }
+};
---
+++ database/migrations/2023_05_12_191652_create_inventories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tool_id');
+            $table->integer('tool_quantity');
+            $table->string('tool_location', 255);
+            $table->string('tool_status', 255);
+            $table->timestamps();
+
+            $table->foreign('tool_id')
+                ->references('id')
+                ->on('tools')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
