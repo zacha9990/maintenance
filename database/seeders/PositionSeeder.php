1795d879efca0bff30739f080bfb8929f5b07627
Z. Hendrik <qtizananto.hendrik@mifx.com>
2023-08-01 06:57:49 +0700

    before stagging

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -18,67 +18,68 @@ class PositionSeeder extends Seeder
     public function run()
     {
         $positions = [
-            'Kepala PGT',
-            'Supervisor Persediaan',
-            'Supervisor Produksi & Mainteance',
-            'Supervisor QC & HSE',
-            'Supervisor Keu, SDM & Umum',
-            'Maintenance',
-            'Administrasi angkutan GT',
-            'Kepala Shift II',
-            'Kepala Shift III',
-            'Kepala Shift I',
-            'Operator Forklift III',
-            'Quality Control Shift I',
-            'Quality Control III',
-            'Keamanan / Security Shift III',
-            'Operator Settler & T Scrubing Shift III',
-            'Penerima Getah',
-            'Operator Settler & T Scrubing Shift II',
-            'Operator Melter Shift II',
-            'Operator Melter Shift I',
-            'Operator Talang Getah & Blow Case Shift I',
-            'Operator Forklift Shift II',
-            'Operator Tengki Pemasak & Tengki Terpentin Shift III',
-            'Operator Tengki Pemasak & Tengki Terpentin Shift I',
-            'Operator Forklift Shift I',
-            'Operator Tengki Pemasak & Tengki Terpentin Shift II',
-            'Operator UPL - Penapis limbah Shift III',
-            'Operator UPL - Penapis limbah Shift I',
-            'Quality Control Shift II',
-            'Operator Boliler Shift I',
-            'Operator Boliler Shift II',
-            'Operator Boliler Shift III',
-            'Staf Keuangan & Data',
-            'Keamanan/Security Shift I',
-            'Cleaning Service (Tukang Kebun)',
-            'Operator Settler & Tengki Scrubing Shift I',
-            'Operator Talang Getah & Blow Case Shift III',
-            'Operator Talang Getah & Blow Case Shift II',
-            'Operator Melter Shift III',
-            'Penerima Getah',
-            'Operator Pengumpan bahan bakar Boiler Shift I',
-            'Operator Pengumpan bahan bakar Boiler III',
-            'Operator UPL - Netralisasi air limbah I',
-            'Operator UPL - Netralisasi air limbah III',
-            'Operator UPL - Netralisasi air limbah II',
-            'Administrasi bahan baku & bahan penolong',
-            'Operator Pengumpan bahan bakar Boiler II',
-            'Keamanan / Security',
-            'Operator UPL - Penapis limbah',
-            'Staf SDM & Umum',
+            // 'Kepala PGT',
+            // 'Supervisor Persediaan',
+            // 'Supervisor Produksi & Mainteance',
+            // 'Supervisor QC & HSE',
+            // 'Supervisor Keu, SDM & Umum',
+            // 'Maintenance',
+            // 'Administrasi angkutan GT',
+            // 'Kepala Shift II',
+            // 'Kepala Shift III',
+            // 'Kepala Shift I',
+            // 'Operator Forklift III',
+            // 'Quality Control Shift I',
+            // 'Quality Control III',
+            // 'Keamanan / Security Shift III',
+            // 'Operator Settler & T Scrubing Shift III',
+            // 'Penerima Getah',
+            // 'Operator Settler & T Scrubing Shift II',
+            // 'Operator Melter Shift II',
+            // 'Operator Melter Shift I',
+            // 'Operator Talang Getah & Blow Case Shift I',
+            // 'Operator Forklift Shift II',
+            // 'Operator Tengki Pemasak & Tengki Terpentin Shift III',
+            // 'Operator Tengki Pemasak & Tengki Terpentin Shift I',
+            // 'Operator Forklift Shift I',
+            // 'Operator Tengki Pemasak & Tengki Terpentin Shift II',
+            // 'Operator UPL - Penapis limbah Shift III',
+            // 'Operator UPL - Penapis limbah Shift I',
+            // 'Quality Control Shift II',
+            // 'Operator Boliler Shift I',
+            // 'Operator Boliler Shift II',
+            // 'Operator Boliler Shift III',
+            // 'Staf Keuangan & Data',
+            // 'Keamanan/Security Shift I',
+            // 'Cleaning Service (Tukang Kebun)',
+            // 'Operator Settler & Tengki Scrubing Shift I',
+            // 'Operator Talang Getah & Blow Case Shift III',
+            // 'Operator Talang Getah & Blow Case Shift II',
+            // 'Operator Melter Shift III',
+            // 'Penerima Getah',
+            // 'Operator Pengumpan bahan bakar Boiler Shift I',
+            // 'Operator Pengumpan bahan bakar Boiler III',
+            // 'Operator UPL - Netralisasi air limbah I',
+            // 'Operator UPL - Netralisasi air limbah III',
+            // 'Operator UPL - Netralisasi air limbah II',
+            // 'Administrasi bahan baku & bahan penolong',
+            // 'Operator Pengumpan bahan bakar Boiler II',
+            // 'Keamanan / Security',
+            // 'Operator UPL - Penapis limbah',
+            // 'Staf SDM & Umum',
             'Teknisi',
-            'SuperAdmin'
+            'SuperAdmin',
+            'Operator'
         ];
 
         foreach ($positions as $position) {
             if ($position === 'Teknisi') {
                 $role = Role::where('name', 'Teknisi')->first();
                 if ($role) {
-                    Position::create([
-                        'name' => $position,
-                        'role_id' => $role->id,
-                    ]);
+                    // Position::create([
+                    //     'name' => $position,
+                    //     'role_id' => $role->id,
+                    // ]);
                 } else {
                     $teknisiRole = Role::create(['name' => 'Teknisi']);
                     Position::create([
@@ -86,17 +87,37 @@ class PositionSeeder extends Seeder
                         'role_id' => $teknisiRole->id,
                     ]);
                 }
-            } else {
-                Position::create(['name' => $position]);
             }
+            // else {
+            //     Position::create(['name' => $position]);
+            // }
+
+            if ($position === 'Operator Pabrik') {
+                $role = Role::where('name', 'Operator')->first();
+                if ($role) {
+                    // Position::create([
+                    //     'name' => $position,
+                    //     'role_id' => $role->id,
+                    // ]);
+                } else {
+                    $teknisiRole = Role::create(['name' => 'Operator']);
+                    Position::create([
+                        'name' => $position,
+                        'role_id' => $teknisiRole->id,
+                    ]);
+                }
+            }
+            // else {
+            //     Position::create(['name' => $position]);
+            // }
 
             if ($position === 'SuperAdmin') {
                 $role = Role::where('name', 'SuperAdmin')->first();
                 if ($role) {
-                    Position::create([
-                        'name' => $position,
-                        'role_id' => $role->id,
-                    ]);
+                    // Position::create([
+                    //     'name' => $position,
+                    //     'role_id' => $role->id,
+                    // ]);
                 } else {
                     $teknisiRole = Role::create(['name' => 'SuperAdmin']);
                     Position::create([
@@ -104,9 +125,10 @@ class PositionSeeder extends Seeder
                         'role_id' => $teknisiRole->id,
                     ]);
                 }
-            } else {
-                Position::create(['name' => $position]);
             }
+            // else {
+            //     Position::create(['name' => $position]);
+            // }
         }
     }
 }
